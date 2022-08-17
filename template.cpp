8fdf042b62de6bea9a26877ad18d914e8aa1a206
sumit-kushwah <sumitkushwah1729@gmail.com>
2022-08-17 08:45:40 +0530

    print answer function added in template

--- template.cpp
+++ template.cpp
@@ -12,7 +12,20 @@ public:
   }
   void run()
   {
-    this->function();
+    auto ans = this->function();
+    this->printAnswer(ans);
+  }
+
+  void printAnswer(auto ans) {
+    string type = typeid(ans).name();
+    if (type == "St6vectorIiSaIiEE") {
+      // for(auto p = ans.begin(); p != ans.end(); p++) {
+      //   cout << *p << " ";
+      // }
+      cout << endl;
+    } else {
+      cout << ans << endl;
+    }
   }
 };
 
