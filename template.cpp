5e1e81f08f7a78d987111a9d7729b2cbc34708ac
sumit kushwah <you@example.com>
2022-09-26 14:27:43 +0530

    converted into simple template

--- template.cpp
+++ template.cpp
@@ -3,34 +3,6 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-  // write your function code here
-  void function() {
-
-  }
-  void run()
-  {
-    auto ans = this->function();
-    this->printAnswer(ans);
-  }
-
-  void printAnswer(auto ans) {
-    string type = typeid(ans).name();
-    if (type == "St6vectorIiSaIiEE") {
-      // for(auto p = ans.begin(); p != ans.end(); p++) {
-      //   cout << *p << " ";
-      // }
-      cout << endl;
-    } else {
-      cout << ans << endl;
-    }
-  }
-};
-
-int main()
-{
-  Solution *s = new Solution();
-  s->run();
+int main() {
+  return 0;
 }
