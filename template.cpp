c99491c95b02055dffe697bbfbb4bdeeddec1ecc
sumit kushwah <you@example.com>
2022-09-29 16:59:16 +0530

    file reading and writing code added

--- template.cpp
+++ template.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
-
 using namespace std;
 
+// write your function here
+
 int main() {
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+
+  
   return 0;
 }
